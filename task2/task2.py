e87d6ad3570430ba82111246b588d9eb301efa5c
2005maitra <143825608+2005maitra@users.noreply.github.com>
2025-08-21 15:30:58 +0530

    Create task2.py

---
+++ task2/task2.py
@@ -0,0 +1,19 @@
+def candies(rating):
+    n = len(rating)
+    if n == 0:
+        return 0
+
+    candies = [1] * n
+
+    for i in range(1, n):
+        if rating[i] > rating[i - 1]:
+            candies[i] = candies[i - 1] + 1
+
+    for i in range(n - 2, -1, -1):
+        if rating[i] > rating[i + 1]:
+            candies[i] = max(candies[i], candies[i + 1] + 1)
+
+    return sum(candies)
+
+
+print(candies([0, 0, 0]))
